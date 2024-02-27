a8ea1d9c29ccb01100a49739f5168c2a542b6e77
Steve Bilogan <steve.bilogan@gmail.com>
2024-02-27 15:03:43 -0500

    rotation

--- ResponsiveMarkupApp/ResponsiveMarkupApp/MainPage.cs
+++ ResponsiveMarkupApp/ResponsiveMarkupApp/MainPage.cs
@@ -1,3 +1,5 @@
+using Windows.Foundation;
+
 namespace ResponsiveMarkupApp;
 
 public sealed partial class MainPage : Page
@@ -7,18 +9,29 @@ public sealed partial class MainPage : Page
 		this
 			.Background(ThemeResource.Get<Brush>("ApplicationPageBackgroundThemeBrush"))
 			.Content(new ResponsiveView()
-				.NarrowTemplate(() => BuildContent(Colors.Green, "Narrow"))
-				.NormalTemplate(() => BuildContent(Colors.Blue, "Normal"))
-				.WideTemplate(() => BuildContent(Colors.Purple, "Wide"))
-				.WidestTemplate(() => BuildContent(Colors.Orange, "Widest"))
+				.NarrowTemplate(() => BuildContent(Colors.Green, "Narrow", 0))
+				.NormalTemplate(() => BuildContent(Colors.Blue, "Normal", 90))
+				.WideTemplate(() => BuildContent(Colors.Purple, "Wide", 180))
+				.WidestTemplate(() => BuildContent(Colors.Orange, "Widest", 270))
 			);
 	}
 
-	private static Grid BuildContent(Color bgColor, string title) =>
+	private static Grid BuildContent(Color bgColor, string title, double rotation) =>
 		new Grid()
+			.RowDefinitions("Auto,Auto")
 			.Background(new SolidColorBrush(bgColor))
-			.Children(new TextBlock()
-				.Text(title)
-				.FontSize(32)
+			.Children(
+				new TextBlock()
+					.Text(title)
+					.FontSize(32),
+				new Image()
+					.Grid(row: 1)
+					.Source("ms-appx:///ResponsiveMarkupApp/Assets/steve.png")
+					.RenderTransformOrigin(new Point(0.5, 0.5))
+					.RenderTransform(
+						new RotateTransform()
+							.Angle(rotation)
+					)
+					.Width(200)
 			);
 }
